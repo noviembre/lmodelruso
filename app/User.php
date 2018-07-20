eba492476273166327d596468f244015d7a28493
Manu Mint Laptop <realgarcilaso@outlook.com>
2018-07-20 05:02:43 -0500

    04.3 User - Creando Metodos

--- app/User.php
+++ app/User.php
@@ -9,6 +9,9 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    const IS_BANNED = 1;
+    const IS_ACTIVE = 0;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -30,6 +33,77 @@ class User extends Authenticatable
     public function posts()
     {
         return $this->hasMany(Post::class);
+    }
+
+    public static function add($fields)
+    {
+        $user = new static;
+        $user->fill($fields);
+        $user->save();
+        return $user;
+    }
+    public function edit($fields)
+    {
+        $this->fill($fields); //name,email
+        $this->save();
+    }
+    public function remove()
+    {
+        //$this->removeAvatar();
+        $this->delete();
+    }
+    public function uploadAvatar($image)
+    {
+        if($image == null) { return; }
+        Storage::delete('/uploads/' . $this->image);
+        $filename = str_random(10) . '.' . $image->extension();
+        $image->saveAs('uploads', $filename);
+        $this->image = $filename;
+        $this->save();
+    }
+    public function getImage()
+    {
+        if($this->avatar == null)
+        {
+            return '/img/no-image.png';
+        }
+        return '/uploads/' . $this->avatar;
+    }
+    public function makeAdmin()
+    {
+        $this->is_admin = 1;
+        $this->save();
+    }
+    public function makeNormal()
+    {
+        $this->is_admin = 0;
+        $this->save();
+    }
+    public function toggleAdmin($value)
+    {
+        if($value == null)
+        {
+            return $this->makeNormal();
+        }
+        return $this->makeAdmin();
+    }
+    public function ban()
+    {
+        $this->status = User::IS_BANNED;
+        $this->save();
+    }
+    public function unban()
+    {
+        $this->status = User::IS_ACTIVE;
+        $this->save();
+    }
+    public function toggleBan($value)
+    {
+        if($value == null)
+        {
+            return $this->unban();
+        }
+        return $this->ban();
     }
 
 }
