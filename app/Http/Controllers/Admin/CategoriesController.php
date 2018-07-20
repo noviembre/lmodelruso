1db2908c8ffbac8f77442d7f51a41f3068ab81c4
Manu Mint Laptop <realgarcilaso@outlook.com>
2018-07-20 17:41:14 -0500

    Admin/Categories CRUD part 1

---
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Http\Controllers\Admin;
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+class CategoriesController extends Controller
+{
+
+    public function index()
+    {
+        $categories = Category::all();
+        return view('admin.categories.index',
+            ['categories'	=>	$categories]);
+    }
+
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,17 @@ Route::get('/', function () {
 
 
 Route::get('/admin', 'Admin\DashboardController@index');
+
+/*
+|--------------------------------------------------------------------------
+| G R U P O   A D M I N
+|--------------------------------------------------------------------------
+|
+| Contiene las rutas que solo el admin puede ver o editar.
+|
+|
+*/
+Route::group(['prefix'=>'admin','namespace'=>'Admin', ], function(){
+    Route::get('/', 'DashboardController@index');
+    Route::resource('/categories', 'CategoriesController');
+});
